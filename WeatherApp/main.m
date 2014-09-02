538ba46a37242dadc47fce6a46ea8495283d19d5
tangbin <tangbineml@163.com>
2014-09-02 15:42:28 +0800

    test2

--- WeatherApp/main.m
+++ WeatherApp/main.m
@@ -7,10 +7,9 @@
 //
 //  Created by tang bin on 13-4-11.
 //  Copyright (c) 2013年 tang bin. All rights reserved.
-//
+// 测试 Jenkins
 
 #import <UIKit/UIKit.h>
-
 #import "AppDelegate.h"
 
 int main(int argc, char *argv[])
